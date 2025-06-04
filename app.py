a5e6a6ec365bed74bc5b146a12352ec0d9fe004e
unknown <leonardocae17@gmail.com>
2025-06-03 22:49:34 -0300

    ajuste requeriments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import scraper  # Módulo que vamos criar para raspar os dados
@@ -46,4 +47,5 @@ def get_exportacao():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)  
